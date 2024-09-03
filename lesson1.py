422c1bc7a87694822fdc7a5cfde7f325fe8555d6
Calderon-Parra <ccalderon-p@cps.k12.il.us>
2024-09-03 12:29:02 -0500

    first commit

---
+++ lesson1.py
@@ -0,0 +1,30 @@
+#this is the pring statement
+#print statement allows us to print out a statement
+#comments are used to help us understand the code
+# print("Hello my name is Cristal")
+# print("I am a student at John Hancock")
+# print("I am learning python")
+#create input for the user
+name = input("What is your name?")
+print("Hello", name)
+#input allows us to take input from the user
+#ask the user for thier age
+#ask the user for their favorite color
+#print out favorite color
+#ask user for their favorite food
+#print out the favorite food
+
+age = input("How old are you?")
+print("I am", age)
+
+color = input("What is your favorite color?")
+print("My favorite color is", color)
+
+food = input("What is your favorite food?")
+print("My favorite food is",food)
+
+#concantenation is the process of combining strings with
+#variables
+#print("You are", age, "years old")
+favorite_animal = input("What is your favorite animal?")
+print("You favorite animal is a" , favorite_animal)
